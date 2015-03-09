7d8ef4279fcc879b52cbecce88bf5d44661df37e
GAURAV  DESHMUKH <gauravkdeshmukh@users.noreply.github.com>
2015-03-08 23:23:43 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,12 +9,17 @@ apt-get install g++
 g++ -c ./src/wordcount.cpp 
 g++ -o Wordcount ./src/wordcount.cpp  
 
+g++ -c ./src/running_median.cpp 
+g++ -o Runningmedian ./src/running_median.cpp 
+
+
 # next I'll make sure that all my programs (written in Python in this example) have the proper permissions
 chmod +x Wordcount
+chmod +x Runningmedian
 
 # finally I'll execute my programs, with the input directory wc_input and output the files in the directory wc_output
 ./Wordcount ./wc_input/test.txt ./wc_output/wc_result.txt
-#python my_running_median.py ./wc_input ./wc_output/med_result.txt
+./Runningmedian ./wc_input/test.txt ./wc_output/med_result.txt
 
-
-
+cat ./wc_output/wc_result.txt
+cat ./wc_output/med_result.txt 
